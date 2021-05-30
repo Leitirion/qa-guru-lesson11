022fff5ccbb4dd222abb6194e9a5ac1d84e49ec0
Ilnar Gilyaev <gilyaev.if@gmail.com>
2021-05-30 20:57:19 +0500

    add faker for lastName, firstName, userNumber, studentEmail, address

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,4 +1,5 @@
 
+import com.github.javafaker.Faker;
 import components.CalendarComponent;
 import org.junit.jupiter.api.Test;
 
@@ -9,24 +10,25 @@ public class FormTest {
     RegistrationPage registrationPage = new RegistrationPage();
     CalendarComponent birthDay = new CalendarComponent();
     OutputModalPage outputModalPage = new OutputModalPage();
+    Faker faker = new Faker();
 
     @Test
     void automationFormTest() {
-        String URL = "https://demoqa.com/automation-practice-form";
-        String firstName = "firstName";
-        String lastName = "lastName";
-        String studentEmail = "userEmail@gmail.com";
-        String userNumber = "1234567890";
-        String dayOfBirth = "01";
-        String monthOfBirth = "April";
-        String yearOfBirth = "2001";
-        String picture = "TestPicture.png";
-        String subject1 = "History";
-        String hobby1 = "Music";
-        String address = "Russia, Ufa.";
-        String state = "NCR";
-        String city = "Noida";
-        String gender = "Female";
+        String URL = "https://demoqa.com/automation-practice-form",
+        firstName = faker.name().firstName(),
+         lastName = faker.name().lastName(),
+         studentEmail = faker.internet().emailAddress(),
+         userNumber = faker.number().digits(10),
+         dayOfBirth = "01",
+         monthOfBirth = "April",
+         yearOfBirth = "2001",
+         picture = "TestPicture.png",
+         subject1 = "History",
+         hobby1 = "Music",
+         address = faker.address().fullAddress(),
+         state = "NCR",
+         city = "Noida",
+         gender = "Female";
 
         registrationPage
                 .openForm(URL)
